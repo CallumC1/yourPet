ceb6988297fd8acc7f86ae87585fdd1f6d390c0c
Callum C <51034701+CallumC1@users.noreply.github.com>
2024-03-04 23:49:08 +0000

    Hero and welcome message changes

--- app/Views/home.php
+++ app/Views/home.php
@@ -7,10 +7,15 @@ require_once( __DIR__ . "../components/navbar.php");
 ?>
 
 <div class="relative w-full max-h-96">
-    <img src="/assets/cow-hero.jpg" alt="" class="w-full h-96 object-cover">
-    <div class="absolute top-12 right-20 z-10 bg-white opacity-[96%] drop-shadow-md w-[28rem] h-72 rounded-sm">
-        <h1 class="text-4xl font-bold text-black p-5">Welcome to <span class="text-green-600 underline">YourPet</span></h1>
-        <p class="text-lg px-5">YourPet is a medical service for pets. We provide medical services for pets and also sell pet products. We are dedicated to providing the best care for your pets.</p>
+    <img src="/assets/images/cow-hero.jpg" alt="" class="w-full h-96 object-cover">
+    <div class="absolute top-12 right-20 z-10 bg-white opacity-[96%] drop-shadow-md w-[28rem] h-72 p-5 rounded-sm">
+        <h1 class="text-4xl font-bold text-black mb-4">Welcome to <span class="text-green-600 underline">YourPet</span></h1>
+        <p class="text-lg mb-4 ml-1">YourPet is a medical service for pets. We provide medical services for pets and also sell pet products. We are dedicated to providing the best care for your pets.</p>
+        
+        <a href="/register" class="group flex justify-center w-60 px-10 py-5 ml-1 bg-green-500 text-black font-semibold text-base">
+            Register Today 
+            <img src="/assets/icons/arrow-right.svg" alt="" class="ml-3 transition-all group-hover:ml-4">
+        </a>
     </div>
 </div>
 
