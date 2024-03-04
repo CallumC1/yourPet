08c9de57a3ccb9b53e17086cf179fcb5869889b9
Callum C <51034701+CallumC1@users.noreply.github.com>
2024-03-04 23:26:31 +0000

    Update home.php with new hero image and welcome message

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,8 +6,12 @@ require_once( __DIR__ . "../components/navbar.php");
 
 ?>
 
-<div class="w-full max-h-80">
-    <img src="/assets/dog-hero.jpg" alt="" class="w-full h-80 object-cover">
+<div class="relative w-full max-h-96">
+    <img src="/assets/cow-hero.jpg" alt="" class="w-full h-96 object-cover">
+    <div class="absolute top-12 right-20 z-10 bg-white opacity-[96%] drop-shadow-md w-[28rem] h-72 rounded-sm">
+        <h1 class="text-4xl font-bold text-black p-5">Welcome to <span class="text-green-600 underline">YourPet</span></h1>
+        <p class="text-lg px-5">YourPet is a medical service for pets. We provide medical services for pets and also sell pet products. We are dedicated to providing the best care for your pets.</p>
+    </div>
 </div>
 
 
