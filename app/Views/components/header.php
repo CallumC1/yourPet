00ba382834d5f211734119e9f373971a7f0899ad
Callum C <51034701+CallumC1@users.noreply.github.com>
2024-03-04 20:38:21 +0000

    Fixed header stylesheet not loading on pages past index

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="..\css\output.css">
+    <link rel="stylesheet" href="\css\output.css">
 
     <!-- Get page title, if not set set to "Your Pet" -->
     <?php $title = isset($pageTitle) ? $pageTitle : "Your Pet" ?>
