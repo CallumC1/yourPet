a6f052f77b6081944698a988e63ea05d45af8f5d
Callum C <51034701+CallumC1@users.noreply.github.com>
2024-03-05 00:14:37 +0000

    Homepage additions

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,9 +1,14 @@
 <?php
+require_once __DIR__ . '/../Models/ProductsModel.php';
 
 
 class HomeController {
     
     public function index() {
+
+        $productsModel = new ProductsModel();
+        $products = $productsModel->getProductsData();
+
         require_once __DIR__ . '../../Views/home.php';
     }
 }
--- app/Views/home.php
+++ app/Views/home.php
@@ -7,16 +7,47 @@ require_once( __DIR__ . "../components/navbar.php");
 ?>
 
 <div class="relative w-full max-h-96">
-    <img src="/assets/images/cow-hero.jpg" alt="" class="w-full h-96 object-cover">
+    <img src="/assets/images/cow-hero.jpg" alt="" class="w-full h-96 object-cover opacity-95">
     <div class="absolute top-12 right-20 z-10 bg-white opacity-[96%] drop-shadow-md w-[28rem] h-72 p-5 rounded-sm">
         <h1 class="text-4xl font-bold text-black mb-4">Welcome to <span class="text-green-600 underline">YourPet</span></h1>
         <p class="text-lg mb-4 ml-1">YourPet is a medical service for pets. We provide medical services for pets and also sell pet products. We are dedicated to providing the best care for your pets.</p>
         
-        <a href="/register" class="group flex justify-center w-60 px-10 py-5 ml-1 bg-green-500 text-black font-semibold text-base">
+        <a href="/register" class="group flex justify-center w-60 px-10 py-5 ml-1 bg-green-600 text-black font-semibold text-base rounded-sm">
             Register Today 
-            <img src="/assets/icons/arrow-right.svg" alt="" class="ml-3 transition-all group-hover:ml-4">
+            <img src="/assets/icons/arrow-right.svg" alt="" class="ml-3 transition-all group-hover:ml-4 size-6">
         </a>
     </div>
+</div>
+
+<div class="flex flex-col w-full">
+
+    <div class="mt-8 flex mx-auto max-w-screen-2xl">
+    
+        <div class="grid grid-cols-3 gap-8 mt-8">
+            <div class="flex flex-col items-center">
+                <img src="/assets/icons/HealthCross.svg" alt="" class="w-20 h-20">
+                <h2 class="text-2xl font-semibold mt-4">Medical Services</h2>
+                <p class="text-lg text-center mt-4">We provide medical services for pets. We have a team of experienced vets and nurses who are dedicated to providing the best care for your pets.</p>
+            </div>
+            <div class="flex flex-col items-center">
+                <img src="/assets/icons/HealthCross.svg" alt="" class="w-20 h-20">
+                <h2 class="text-2xl font-semibold mt-4">Pet Food</h2>
+                <p class="text-lg text-center mt-4">We sell a variety of pet food. We have food for all types of pets. We also have food for pets with special dietary needs.</p>
+            </div>
+            <div class="flex flex-col items-center">
+                <img src="/assets/icons/HealthCross.svg" alt="" class="w-20 h-20">
+                <h2 class="text-2xl font-semibold mt-4">Medicine</h2>
+                <p class="text-lg text-center mt-4">We sell a variety of pet medicine. We have medicine for all types of pets. We also have medicine for pets with special medical needs.</p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="mt-20  mx-auto max-w-screen-2xl">
+    
+        <h1 class="font-semibold text-4xl">Pet care</h1>
+        <p>Get top 6 product cards to display here</p>
+    </div>
+
 </div>
 
 
