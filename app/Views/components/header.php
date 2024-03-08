b73e3442c8b9c50a98d1eccf200769f10bb6a8bc
callumC1 <51034701+CallumC1@users.noreply.github.com>
2024-03-08 18:20:06 +0000

    Remove session start from header.php as it is now in index

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -1,7 +1,4 @@
 <?php
-// Session start
-session_start();
-
 // Get page title, if not set set to "Your Pet"
 $title = isset($pageTitle) ? $pageTitle : "Your Pet";
 $background = isset($pageBackground) ? $pageBackground : "bg-slate-100";
