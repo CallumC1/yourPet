cfb65db6ccc38916386032b1625dd5980dba09b7
Callum C <51034701+CallumC1@users.noreply.github.com>
2024-04-15 20:33:38 +0100

    404 pagen now has cute doggo on it. 10/10 would pet.

--- app/Views/404.php
+++ app/Views/404.php
@@ -1,5 +1,20 @@
+<?php
 
-<h1>404 - Page not found.</h1>
-<p>Sorry, the page you are looking for does not exist.</p>
+require_once( __DIR__ . "../components/header.php");
 
-<a href="/">Go back home</a>
+?>
+
+<div class="flex flex-col h-screen w-screen mx-auto justify-center items-center">
+    <div class="size-80">
+        <img src="/assets/images/404.webp" alt="Cute Dog holding Page Not Found sign" class="w-80 h-80">
+    </div>
+
+    <h1>404 - Page not found.</h1>
+    <p class="text-lg">Sorry, the page you are looking for does not exist.</p>
+    <a href="/" class="text-xl text-blue-500 underline">Go back home</a>
+</div>
+
+
+
+</body>
+</html>
