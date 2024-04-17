fa610b609653343a53194fad5afd4fc4f453a09c
Callum C <51034701+CallumC1@users.noreply.github.com>
2024-04-17 23:35:45 +0100

    Changed the dashboard cards styling

--- app/Views/components/dashboardCard.php
+++ app/Views/components/dashboardCard.php
@@ -1,27 +1,18 @@
 
 <!-- My Pets Card -->
-<div class="flex flex-col gap-y-3 bg-white w-full md:w-80 h-auto p-3 ring-emerald-500 duration-200 hover:ring-2 pointer-events-none drop-shadow-md rounded-md">
+<a href="<?= $link ?>" class="group flex flex-col gap-y-3 bg-white w-full md:w-80 h-auto px-3 py-6 ring-emerald-500 duration-200 hover:ring-2 drop-shadow-md rounded-md cursor-pointer">
 
-    <div class="flex gap-x-4 items-center">
+    <div class="flex gap-x-4 items-center size-full">
 
-        <!-- Might look better without icons -->
-        <span class="w-14 h-14 bg-[#109DB9] rounded-md flex justify-center items-center ">
-            <img src="/assets/icons/user.svg" alt="Profile" class="size-8" /> <!-- CUSTOM TO DO -->
-        </span>
-
-        <span class="flex flex-col ml-3">
-            <h2><?= $title ?></h2> 
+        <div class="flex flex-col w-full">
+            <span class="flex items-center justify-between">
+                <h2><?= $title ?></h2> 
+                <img src="/assets/icons/arrow-right-black.svg" alt="Right Arrow" class="size-6 mr-1 group-hover:translate-x-1 transition-transform"> 
+            </span>
+            
             <p><?= $description ?></p> 
-        </span>
+        </div>
 
     </div>
 
-    <!-- Button / Link -->
-    <a href="<?= $link ?>"  class="group text-white font-semibold  bg-emerald-500 w-full px-2 py-3 mx-auto rounded-md pointer-events-auto hover:bg-emerald-600">
-        <span class="flex">
-            <?= $linkText ?>
-            <img src="/assets/icons/arrow-right.svg" alt="Right Arrow" class="size-6 ml-2 group-hover:translate-x-1 transition-transform"> 
-        </span>
-    </a> 
-
-</div>
+</a>
