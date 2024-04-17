1adf3f54fbbf936efae84852ce5c25aefb28d0ff
callumC1 <51034701+CallumC1@users.noreply.github.com>
2024-04-17 19:32:01 +0100

    Added more options to dash

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -7,24 +7,24 @@ require_once( __DIR__ . "../components/navbar.php");
 
 
 <!-- Page Container -->
-<div class="flex flex-col w-full px-3 mt-8 max-w-screen-2xl mx-auto">
+<div class="flex flex-col w-full px-4 mt-6 max-w-screen-2xl mx-auto">
 
-    <div class="bg-[#109DB9] w-full h-60 mx-3 p-4 rounded-sm text-white">
+    <div class="bg-[#109DB9] w-full h-auto p-4 rounded-sm text-white">
         <h1>User Dashboard</h1>
         <p>Welcome to your dashboard, <?php out($_SESSION["user_data"]["name"]) ?> </p>
     </div>
 
 
-    <div class="flex flex-col md:flex-row gap-8 p-3 w-full h-full ">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto gap-8 py-3 w-full h-full items-center place-items-center ">
 
 
 
-        <!-- Card -->
-        <div class="flex flex-col gap-y-3 bg-white w-80 h-36 rounded-md p-3 ring-emerald-500 duration-200 hover:ring-2 pointer-events-none drop-shadow-md">
+        <!-- My Pets Card -->
+        <div class="flex flex-col gap-y-3 bg-white w-full md:w-80 h-auto rounded-md p-3 ring-emerald-500 duration-200 hover:ring-2 pointer-events-none drop-shadow-md">
 
             <div class="flex gap-x-4 items-center">
 
-                <span class="size-14 bg-[#109DB9] rounded-md flex justify-center items-center ">
+                <span class="w-14 h-14 bg-[#109DB9] rounded-md flex justify-center items-center ">
                     <img src="/assets/icons/user.svg" alt="Profile" class="size-8" />
                 </span>
         
@@ -35,16 +35,16 @@ require_once( __DIR__ . "../components/navbar.php");
 
             </div>
 
-            <a href="#" class="bg-emerald-500 w-full  mx-auto px-2 py-3 rounded-md text-white font-semibold text-center pointer-events-auto">Pets</a>
+            <a href="/pets" class="bg-emerald-500 w-full  mx-auto px-2 py-3 rounded-md text-white font-semibold text-center pointer-events-auto">Pets</a>
 
         </div>
 
-        <!-- Card -->
-        <div class="flex flex-col gap-y-3 bg-white w-80 h-36 rounded-md p-3 ring-emerald-500 duration-200 hover:ring-2 pointer-events-none drop-shadow-md">
+        <!-- Appointments Card -->
+        <div class="flex flex-col gap-y-3 bg-white w-full md:w-80 h-auto rounded-md p-3 ring-emerald-500 duration-200 hover:ring-2 pointer-events-none drop-shadow-md">
 
             <div class="flex gap-x-4 items-center">
 
-                <span class="size-14 bg-[#109DB9] rounded-md flex justify-center items-center ">
+                <span class="w-14 h-14 bg-[#109DB9] rounded-md flex justify-center items-center ">
                     <img src="/assets/icons/user.svg" alt="Profile" class="size-8" />
                 </span>
         
@@ -59,13 +59,13 @@ require_once( __DIR__ . "../components/navbar.php");
 
         </div>
 
-        <!-- Card -->
-        <div class="flex flex-col gap-y-3 bg-white w-80 h-36 rounded-md p-3 ring-emerald-500 duration-200 hover:ring-2 pointer-events-none drop-shadow-md">
+        <!-- My Profile Card -->
+        <div class="flex flex-col gap-y-3 bg-white w-full md:w-80 h-auto rounded-md p-3 ring-emerald-500 duration-200 hover:ring-2 pointer-events-none drop-shadow-md">
 
             <div class="flex gap-x-4 items-center">
 
-                <span class="size-14 bg-[#109DB9] rounded-md flex justify-center items-center ">
-                    <img src="/assets/icons/user.svg" alt="Profile" class="size-8" />
+                <span class="w-14 h-14 bg-[#109DB9] rounded-md flex justify-center items-center ">
+                    <img src="/assets/icons/user.svg" alt="Profile" class="size-10" />
                 </span>
 
                 <span class="flex flex-col ml-3">
@@ -76,6 +76,26 @@ require_once( __DIR__ . "../components/navbar.php");
             </div>
 
             <a href="#" class="bg-emerald-500 w-full  mx-auto px-2 py-3 rounded-md text-white font-semibold text-center pointer-events-auto">Manage Profile</a>
+
+        </div>
+        
+        <!-- Orders Card -->
+        <div class="flex flex-col gap-y-3 bg-white w-full md:w-80 h-auto rounded-md p-3 ring-emerald-500 duration-200 hover:ring-2 pointer-events-none drop-shadow-md">
+
+            <div class="flex gap-x-4 items-center">
+
+                <span class="w-14 h-14 bg-[#109DB9] rounded-md flex justify-center items-center ">
+                    <img src="/assets/icons/user.svg" alt="Orders" class="size-10" />
+                </span>
+
+                <span class="flex flex-col ml-3">
+                    <h2>My Orders</h2>
+                    <p>View your orders</p>
+                </span>
+
+            </div>
+
+            <a href="#" class="bg-emerald-500 w-full  mx-auto px-2 py-3 rounded-md text-white font-semibold text-center pointer-events-auto">Orders</a>
 
         </div>
 
