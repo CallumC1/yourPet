f036b12b2aa85eaa9aec16ff54b2b3c7256998a1
Callum C <51034701+CallumC1@users.noreply.github.com>
2024-04-18 12:26:23 +0100

    Add pet card to mypets.php

--- app/Views/mypets.php
+++ app/Views/mypets.php
@@ -22,7 +22,50 @@ require_once( __DIR__ . "../components/navbar.php");
     </div>
 
 
-    test
+    <!-- Card -->
+    <div class="bg-white p-2 w-96">
+        <div class="flex gap-3">
+
+            <div class="flex flex-shrink-0 overflow-hidden rounded-sm w-32 h-auto ">
+                <img src="/assets/images/isla.jpg" alt="" class="object-cover">
+            </div>
+
+            <div class="flex flex-col w-full">
+                <h2>Isla</h2>
+
+                <span class="flex justify-between text-sm">
+                    <p>Age:</p>
+                    <p>3</p>
+                </span>
+
+                <span class="flex justify-between text-sm">
+                    <p>Sex:</p>
+                    <p>Female</p>
+                </span>
+
+                <span class="flex justify-between text-sm">
+                    <p>Breed:</p>
+                    <p>Cockapoo</p>
+                </span>
+
+                <a href="#" class="bg-emerald-500 text-white w-ful p-2 mt-3 rounded-sm">
+                    View Details
+                </a>
+
+            </div>
+
+        </div>
+    </div>
+    <!-- End Card -->
+
+
+    <!-- Add pet card -->
+
+    <div>
+        
+    </div>
+
+
 </div>
 
 <?php 
