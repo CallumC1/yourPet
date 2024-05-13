0b1b2be9c79064c665a2fa786c56e19673318ec2
callumC1 <51034701+CallumC1@users.noreply.github.com>
2024-05-13 19:21:03 +0100

    chore: Update productCard.php to use PHP out() function for outputting variables

--- app/Views/components/productCard.php
+++ app/Views/components/productCard.php
@@ -1,5 +1,5 @@
 <a class="p-4 w-full lg:w-64 bg-white drop-shadow-md" href="/products/<?=$product_type?>"> 
-    <h3 class="font-semibold"><?= $product_name ?></h3>
-    <p>Type: <?= $product_type ?></p>
-    <p>Stock: <?= $product_stock ?></p>
+    <h3 class="font-semibold"><?php out( $product_name ) ?></h3>
+    <p>Type: <?php out( $product_type )?></p>
+    <p>Stock: <?= out( $product_stock  )?></p>
 </a>
