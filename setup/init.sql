85914e80b389f7d30a2ae57d69d7e0f2726b0b5a
Callum C <51034701+CallumC1@users.noreply.github.com>
2024-05-23 14:20:35 +0100

    fix for db not loading

--- setup/init.sql
+++ setup/init.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2024 at 12:21 AM
+-- Generation Time: May 23, 2024 at 02:59 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
